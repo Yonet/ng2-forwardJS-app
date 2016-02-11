d233618fc99216a257a96d8f727ce68de2c80508
Aysegul Yonet <aysegul.yonet@autodesk.com>
2016-02-10 16:32:37 -0800

    ngFor example added.

--- app/Projects/projects.component.ts
+++ app/Projects/projects.component.ts
@@ -15,13 +15,28 @@ interface Project {
 })
 
 export class ProjectsComponent {
-    /**
-     * Variable name is 'project' and type is 'Project' defined in the interface
-     * @type {{id: number, name: string}}
-     */
-    public project: Project = {
-        id: 1,
-        name: "Demo Project"
+    public title = "Projects List";
+    public projects = PROJECTS;
+    public newProject = '';
+
+    onSubmit(newName){
+        const i = this.projects.length + 1;
+        this.projects.push({id: i, name: newName});
+        console.log('newProject is', this.newProject);
+        this.newProject = '';
     }
 
 }
+
+/**
+ * Projects array with the type project
+ * @type {{id: number, name: string}[]}
+ */
+var PROJECTS: Project[] = [
+    {id: 1, name: "Class Intro"},
+    {id: 2, name: "HTML"},
+    {id: 3, name: "CSS"},
+    {id: 4, name: "JS"},
+    {id: 5, name: "JQuery"},
+    {id: 6, name: "Angular"}
+];
